56e40f8e45ab023322cdfa24d5db713fe99b79ea
Icrai <104451848+Icrai@users.noreply.github.com>
2022-04-26 13:11:23 -0400

    Add files via upload

---
+++ Joobis Hungis/Source/Class1.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joobis_Hungis
+{
+    public class ThingDef_EnergyDrink : ThingDef
+    {
+    }
+}
